0df3e1a8cfdfffc63afc4f3de88aad4b0adf3e77
nurzhan <Salikh835@gmail.com>
2023-02-24 19:00:35 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -3,5 +3,11 @@ package school.mjc.stage0.loops.task4;
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
 
+        for (int i = 0; i < lastInRow; i++) {
+            if (i == numberToSkip) {
+                continue;
+            }
+            System.out.println(i+" ");
+        }
     }
 }
