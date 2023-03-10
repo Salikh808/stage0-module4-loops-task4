a2a88a227882f3a8226badae8c8f9f76fda3d5cb
nurzhan <Salikh835@gmail.com>
2023-03-10 17:27:30 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -1,13 +1,15 @@
 package school.mjc.stage0.loops.task4;
 
-public class SkippingThird {
-    public void printUntilButThird(int lastPrinted) {
+    public class SkippingThird {
+        public void printUntilButThird(int lastPrinted) {
 
-        for (int i = 0; i < lastPrinted; i++) {
-            if (i % 3 == 0) {
-                continue;
+            for (int i = 0; i < lastPrinted; i++) {
+
+                if (!(i % 3 == 0)) {
+                    System.out.println(i);
+
+                }
             }
-            System.out.println(i+" ");
         }
     }
-}
+
